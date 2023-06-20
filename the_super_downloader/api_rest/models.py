1a354377c0fc80b4d1ff7a7ea5b5f708ae499f75
Leonardo Guido <lguido@epochteca.com>
2023-06-19 21:56:15 -0600

    Base models created

---
+++ the_super_downloader/api_rest/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Format(models.Model):
+    name = models.CharField(max_length = 100 , null = False, blank = False)
+    extensions = models.CharField(max_length=500, null = False, blank = False) #dumped JSON contains: string list with extensions Ex. (".mp4")
+
+class Download(models.Model):
+
+    format = models.ForeignKey(Format, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50 , null=False, blank=False)
+    date = models.DateTimeField(auto_now=True, auto_now_add=True, null=False, blank=False)
+    key = models.CharField(max_length=250, null=False, blank=False)
+    miniature_key = models.CharField(max_length=250, null=True, blank=True)
+
+    #optional
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
